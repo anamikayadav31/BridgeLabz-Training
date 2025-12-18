3354c95d97d9e247d44aee3fa50b2c64034f7b79
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2025-12-18 15:01:04 +0530

    Remove core-java-practice folder and its contents

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AddTwoNumbers.cs
@@ -0,0 +1,13 @@
+using System;
+class AddTwoNumbers{
+	public static void Main(string[] args){
+		//Ask user to input first number
+		int a=int.Parse(Console.ReadLine());
+		//Ask user to input second number
+		int b=int.Parse(Console.ReadLine());
+		//add two numbers and result store in x
+		int x=a+b;
+		//print x
+		Console.WriteLine(x);
+	}
+}
---
+++ core-csharp-practice/gcr-codebase/programming-elements/PowerCalculation.cs
@@ -0,0 +1,13 @@
+using System;
+class  PowerCalculation{
+	public static void Main(string[] args){
+		//take base as input
+		int a=4;
+		//take exponent as input
+		int b=5;
+		//calculate power
+		double p=Math.Pow(a,b);
+		//print result
+		Console.WriteLine(p);
+	}
+}
