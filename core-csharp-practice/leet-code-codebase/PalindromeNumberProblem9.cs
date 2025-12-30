b92dff12b895f4131e8488cba29b19ac54854c8c
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2025-12-30 15:07:43 +0530

    initial commit

---
+++ core-csharp-practice/leet-code-codebase/PalindromeNumberProblem9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.leetcode
+{
+    internal class PalindromeNumberProblem9
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int temp = n;
+            while (n != 0)
+            {
+                int rem = n % 10;
+                sum = sum * 10 + rem;
+                n = n / 10;
+            }
+
+            if (temp == sum)
+            {
+                Console.WriteLine("It is a Palindrome Number");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Palindrome Number");
+            }
+        }
+
+
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/class-object-problems/BookCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.classAndObject
+{
+    internal class BookCreate
+    {
+        static void Main()
+        {
+            // Take book details from the user
+            Console.Write("Enter Book Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Enter Author Name: ");
+            string author = Console.ReadLine();
+
+            Console.Write("Enter Book Price: ");
+            double price = double.Parse(Console.ReadLine());
+            //create object
+            Book book = new Book(title, author, price);
+            Console.WriteLine("Title: " + book.Title);
+            Console.WriteLine("Author: " + book.Author);
+            Console.WriteLine("Price: Rs" + book.Price);
+        }
+    }
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public double Price;
+
+        public Book(string title, string author, double price)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+
+
+
+    }
+}
+
+    
