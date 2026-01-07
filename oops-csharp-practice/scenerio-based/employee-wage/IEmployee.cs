b5d5c3804d5c78c933478b0d9c206099c94da003
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-07 16:46:06 +0530

    initial commit

---
+++ oops-csharp-practice/scenerio-based/employee-wage/IEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.sceneriobased.employeeWage
+{
+
+
+    // Interface for employee-related operations
+    internal interface IEmployee
+    {
+        // Method to add employee and calculate wage
+        void AddEmployee();
+    }
+}
