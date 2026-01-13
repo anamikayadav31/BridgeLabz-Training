8a7d62bcf2d136bbad629b8abbf88fa54d9137bc
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-13 16:44:36 +0530

    initial commit

---
+++ oops-csharp-practice/scenerio-based/book-buddyy/BookShelfMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.sceneriobased.BookBuddy
+{
+    internal class BookShelfMenu
+    {
+        public  void ShelfMenu()
+        {
+            BookShelfUtility utility = new BookShelfUtility();
+            string choice = "";
+            while (choice != "4")
+            {
+                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("Enter 1:Add books");
+                Console.WriteLine("Enter 2:Search books");
+                Console.WriteLine("Enter 3:Sort books");
+                Console.WriteLine("Enter 4:Exit");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        utility.addBook();
+                        break;
+                    case "2":
+                        utility.searchBook();
+                        break;
+                    case "3":
+                        utility.sortBooks();
+                        break;
+                    case "4":
+                       break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ oops-csharp-practice/scenerio-based/book-buddyy/IBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.oops.sceneriobased.BookBuddy
+{
+    internal interface IBook
+    {
+        void addBook();
+        void sortBooks();
+        void searchBook();
+    }
+}
