fa6e185b58820a3514f9605026ef8ced8378a866
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-13 16:32:15 +0530

    initial commit

---
+++ dsa-csharp-practice/dsa-linear-binary-search/ConcatenateStringsUsingStringBuilder.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzTraining.DataStructuresAndAlgorithm.StringBuilderBinarySearchLinearSearch
+//{
+//    internal class ConcatenateStringsUsingStringBuilder
+//    {
+       
+
+//        static void Main()
+//        {
+//            // Number of strings to concatenate
+//            Console.Write("Enter number of strings: ");
+//            int n = int.Parse(Console.ReadLine());
+
+//            // StringBuilder for efficient concatenation
+//            StringBuilder sb = new StringBuilder();
+
+//            // Read strings one by one
+//            for (int i = 0; i < n; i++)
+//            {
+//                Console.Write("Enter string: ");
+//                sb.Append(Console.ReadLine()); // Append each string
+//            }
+
+//            // Output final string
+//            Console.WriteLine("Concatenated String: " + sb);
+//        }
+//    }
+
+//}
---
+++ dsa-csharp-practice/dsa-linear-binary-search/RemoveDuplicatesUsingStringBuilder.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzTraining.DataStructuresAndAlgorithm.StringBuilderBinarySearchLinearSearch
+//{
+//    internal class RemoveDuplicatesUsingStringBuilder
+//    {
+
+//        static void Main()
+//        {
+//            // Read input string
+//            Console.Write("Enter string: ");
+//            string input = Console.ReadLine();
+
+//            // StringBuilder to store unique characters
+//            StringBuilder result = new StringBuilder();
+
+//            // Loop through each character of input
+//            for (int i = 0; i < input.Length; i++)
+//            {
+//                bool isDuplicate = false;
+
+//                // Check if character already exists in result
+//                for (int j = 0; j < result.Length; j++)
+//                {
+//                    if (input[i] == result[j])
+//                    {
+//                        isDuplicate = true;
+//                        break;
+//                    }
+//                }
+
+//                // Add character only if not duplicate
+//                if (!isDuplicate)
+//                {
+//                    result.Append(input[i]);
+//                }
+//            }
+
+//            // Display final string
+//            Console.WriteLine("After removing duplicates: " + result);
+//        }
+//    }
+//}
