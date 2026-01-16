80647f2394328519bcbe2800eb40edee8ed0c99a
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-16 16:29:12 +0530

    add traffic-manager

---
+++ dsa-csharp-practice/scenerio-based/traffic-manager/TrafficMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.trafficmanger
+{
+    internal class TrafficMain
+    {
+        static void Main()
+        {
+            TrafficMenu fun = new TrafficMenu();
+            fun.ShowMenu();
+        }
+    }
+}
