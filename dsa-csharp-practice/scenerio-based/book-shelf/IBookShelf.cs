9ce275f477710b377fe49f4c2c1a3826a1cbcc96
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-16 16:04:03 +0530

    Add book-shelf

---
+++ dsa-csharp-practice/scenerio-based/book-shelf/IBookShelf.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.bookShelf
+//{
+//    internal interface IBookShelf
+//    {
+
+
+//            void AddBook();
+//            void BorrowBook();
+//            void DisplayLibrary();
+        
+//    }
+//}
