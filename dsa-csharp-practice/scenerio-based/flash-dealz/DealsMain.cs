78f7d0589d157429a2324ee0cf7d21e16a0c2258
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-17 14:41:00 +0530

    Add flash-dealz

---
+++ dsa-csharp-practice/scenerio-based/flash-dealz/DealsMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.FlashDealz
+{
+    internal class DealsMain
+    {
+        static void Main()
+        {
+            DealsMenu menu=new DealsMenu();
+            Console.WriteLine("Welcome to FlashDealz:Product Sorting by Discount");
+            menu.ShowMenu();
+
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenerio-based/flash-dealz/IDeal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.FlashDealz
+{
+    internal interface IDeal
+    {
+        void AddProductDetails();
+        void QuickSort();
+        void DisplayProducts();
+
+    }
+}
