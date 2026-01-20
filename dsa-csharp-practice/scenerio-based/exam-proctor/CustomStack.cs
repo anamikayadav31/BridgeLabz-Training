342c897a7ff6f6903414db18443b2984f09e0d02
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-20 11:43:04 +0530

    initial commit

---
+++ dsa-csharp-practice/scenerio-based/exam-proctor/CustomStack.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.examProctor
+{
+    internal class CustomStack
+    {
+    }
+}
