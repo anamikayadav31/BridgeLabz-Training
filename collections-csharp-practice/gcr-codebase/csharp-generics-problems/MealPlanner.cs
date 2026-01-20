5984a06a75acc439af2246621d313e7a7806b5bb
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-20 15:33:26 +0530

    initial commit

---
+++ collections-csharp-practice/gcr-codebase/csharp-generics-problems/MealPlanner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Collections.generics_problems
+{
+
+    interface IMealPlan
+    {
+        string GetMeal(); // Returns meal type
+    }
+
+    // Vegetarian meal implementation
+    class VegetarianMeal : IMealPlan
+    {
+        public string GetMeal()
+        {
+            return "Vegetarian Meal";
+        }
+    }
+
+    // Vegan meal implementation
+    class VeganMeal : IMealPlan
+    {
+        public string GetMeal()
+        {
+            return "Vegan Meal";
+        }
+    }
+
+    // Generic Meal class with constraints
+    class Meal<T> where T : IMealPlan, new()
+    {
+        // Creates and displays meal
+        public void GenerateMeal()
+        {
+            T meal = new T();          // new() constraint
+            Console.WriteLine(meal.GetMeal());
+        }
+    }
+
+    class MealPlanner
+    {
+        static void Main()
+        {
+            // Generate vegetarian meal
+            Meal<VegetarianMeal> vegMeal = new Meal<VegetarianMeal>();
+            vegMeal.GenerateMeal();
+
+            // Generate vegan meal
+            Meal<VeganMeal> veganMeal = new Meal<VeganMeal>();
+            veganMeal.GenerateMeal();
+        }
+    }
+
+}
