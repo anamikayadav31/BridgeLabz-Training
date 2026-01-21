48ee9de4150c4442404ccb8258de8fb5e05d2b95
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-21 16:25:23 +0530

    Add Edu-Result

---
+++ dsa-csharp-practice/scenerio-based/edu-results/IResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.DataStructuresAndAlgorithm.scenerioBased.EduResults
+{
+    internal interface IResult
+    {
+    }
+}
