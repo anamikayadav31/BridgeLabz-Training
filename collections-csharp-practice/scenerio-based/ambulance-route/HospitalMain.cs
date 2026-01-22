208b4fe5783ddfe54b7e88305255cdfee8ffedb8
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-01-22 16:50:01 +0530

    initial commit

---
+++ collections-csharp-practice/scenerio-based/ambulance-route/HospitalMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Collections.scnerio_based.ambulance_route
+{
+    internal class HospitalMain
+    {
+        // Entry point of the application
+        static void Main()
+        {
+            // Create menu object
+            HospitalMenu menu = new HospitalMenu();
+
+            // Display hospital menu
+            menu.ShowMenu();
+        }
+    }
+}
