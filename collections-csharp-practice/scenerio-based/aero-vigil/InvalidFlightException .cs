339072c7cddf7adddaa6561b7026f4c110b807f4
anamikayadav31 <anamika.yadav_cs22@gla.ac.in>
2026-02-18 22:25:02 +0530

    Removed collections folder from IO branch

--- collections-csharp-practice/scenerio-based/aero-vigil/InvalidFlightException .cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-// Custom exception for invalid flight details
-public class InvalidFlightException : Exception
-{
-    public InvalidFlightException(string message) : base(message) { }
-}
